2f84d4716043e580a4585168c06c8ed41a03fbd9
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-28 14:32:07 -0700

    hot fix

--- main.h
+++ main.h
@@ -77,7 +77,7 @@ int print_pointer(va_list types, char buffer[], int flags, int width,
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
+int get_size(const char *format, int *i, va_list list);
 
 /* Functions to print strings in reverse */
 int print_reverse(va_list types, char buffer[], int flags, int width,
